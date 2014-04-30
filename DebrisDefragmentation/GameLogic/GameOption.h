b6135b9087824773c6606596ebf1ce410e79b492
NHNNEXT_1 Choi Gyeongwook <wooq@nhnnext.org>
2014-04-30 13:26:41 +0900

    모델 교체에 따른 캐릭터 충돌 박스 상수값 변경

--- DebrisDefragmentation/GameLogic/GameOption.h
+++ DebrisDefragmentation/GameLogic/GameOption.h
@@ -9,13 +9,13 @@ const float			MOUSE_ROTATION_WEIGHT = 0.1f;
 const int			VECTOR_DIRECTION_3 = 3;
 const int			BOX_POINT_COUNT = 8;
 
-const float			CHARACTER_CB_LENGTH_X = 1.0f;
+const float			CHARACTER_CB_LENGTH_X = 0.5f;
 const float			CHARACTER_CB_LENGTH_Y = 1.0f;
-const float			CHARACTER_CB_LENGTH_Z = 1.0f;
+const float			CHARACTER_CB_LENGTH_Z = 0.65f;
 
 const float			CHARACTER_CB_CENTER_POS_X = 0.0f;
-const float			CHARACTER_CB_CENTER_POS_Y = 0.0f;
-const float			CHARACTER_CB_CENTER_POS_Z = 0.0f;
+const float			CHARACTER_CB_CENTER_POS_Y = -5.5f;
+const float			CHARACTER_CB_CENTER_POS_Z = -1.5f;
 
 // 추진체 관련
 const int			OXYGEN_COUNSUMED = 1;
