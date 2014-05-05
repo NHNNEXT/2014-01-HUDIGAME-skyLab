0581cbbf6b8bc04ee79efb568ba5c187fd469867
NHNNEXT_1 Choi Gyeongwook <wooq@nhnnext.org>
2014-05-05 19:54:33 +0900

    커밋 중 충돌 해결

--- DebrisDefragmentation/GameLogic/GameOption.h
+++ DebrisDefragmentation/GameLogic/GameOption.h
@@ -17,6 +17,9 @@ enum class ClientUITag
 	UI_FRAME_TAG,
 	UI_TAG_COUNT // total UI 숫자
 };
+
+
+// ISS
 enum class TeamColor
 {
 	NO_TEAM = -1,
@@ -29,6 +32,7 @@ enum class ISSModuleName
 	// 이름 의미있는 걸로 바꾸는 게 좋을까
 	// 하지만 실제 ISS 이름은 PART1보다 더 이해하기 어려운 이름인 게 함정 ㅋㅋ
 	NO_MODULE = -1,
+	PART0,
 	PART1,
 	PART2,
 	PART3,
@@ -36,9 +40,13 @@ enum class ISSModuleName
 	PART5,
 	PART6,
 	PART7,
-	PART8
 };
 const unsigned int	MODULE_NUMBER = 8;
+
+const float ISS_MOVE_WEIGHT = 5.0f;
+
+// 각 모듈의 초기 위치(모델), 충돌상자 중심점, 충돌상자의 각 축에 대한 길이, 
+
 
 // 게임 셋팅
 const unsigned int	MAX_PLAYER_NUM = 8;
@@ -48,6 +56,12 @@ const unsigned int	ACCELERATION_TIME = 500;
 
 const int			VECTOR_DIRECTION_3 = 3;
 const int			BOX_POINT_COUNT = 8;
+
+#define				BLUE_TEAM_POSITION	D3DXVECTOR3(100.0f, .0f, .0f)
+#define				RED_TEAM_POSITION	D3DXVECTOR3(-100.0f, .0f, .0f)
+#define				BLUE_TEAM_ROTATION	D3DXVECTOR3(.0f, 270.0f, .0f)
+#define				RED_TEAM_ROTATION	D3DXVECTOR3(.0f, 90.0f, .0f)
+
 
 // collision box
 /*
@@ -63,17 +77,10 @@ const float			CHARACTER_CB_LENGTH_X = 1.0f;
 const float			CHARACTER_CB_LENGTH_Y = 1.0f;
 const float			CHARACTER_CB_LENGTH_Z = 1.0f;
 
-#define				BLUE_TEAM_POSITION	D3DXVECTOR3(100.0f, .0f, .0f)
-#define				RED_TEAM_POSITION	D3DXVECTOR3(-100.0f, .0f, .0f)
-#define				BLUE_TEAM_ROTATION	D3DXVECTOR3(.0f, 270.0f, .0f)
-#define				RED_TEAM_ROTATION	D3DXVECTOR3(.0f, 90.0f, .0f)
-
-
-
-
 const float			CHARACTER_CB_CENTER_POS_X = 0.0f;
 const float			CHARACTER_CB_CENTER_POS_Y = -0.5f;
 const float			CHARACTER_CB_CENTER_POS_Z = 0.0f;
+
 
 // 추진체 관련
 const int			OXYGEN_COUNSUMED = 1;
@@ -84,8 +91,10 @@ const int			DEFAULT_OXYGEN = 3000;
 const int			DEFAULT_FUEL = 5000;
 const int			DEFAULT_HP = 100;
 
+
 // SKILL
 const float			SKILL_RANGE = 50;
+
 
 // UI
 const float			UI_OXYGEN_POSITION_X = 10.0f;
@@ -103,16 +112,19 @@ const std::array<std::wstring, size_t( ClientUITag::UI_TAG_COUNT )> UI_FILENAME_
 		L"Frame.png"
 	};
 
+
 // Config
 const bool			MOUSE_POINTER_ON = true;
 const bool			USE_LOCAL_SERVER = true;
 const float			MOUSE_ROTATION_SENSITIVITY = 0.1f;
+
 
 // KEY MAPPING
 const int			VK_W = 0x57;
 const int			VK_S = 0x53;
 const int			VK_1 = 0x31;
 const int			VK_2 = 0x32;
+
 
 // Axis Set
 #define				ZERO_VECTOR3		D3DXVECTOR3( 0.0f, 0.0f, 0.0f )
