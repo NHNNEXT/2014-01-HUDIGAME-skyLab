6b790336457790dd387badcb34aa2ed3ee342704
1기 문진상 <blastingzone@naver.com>
2014-05-18 20:14:10 +0900

    Config.json 파일 업데이트

--- DebrisDefragmentation/GameTool/Class/JsonEnum.cs
+++ DebrisDefragmentation/GameTool/Class/JsonEnum.cs
@@ -1,19 +1,2 @@
-﻿// C#이랑 C++에서 동시에 JSON 키 값을 사용하기 위한 파일
+﻿
 
-///# 헐.... 라인 매크로는 C#에 없으니까 이렇게 한겨?
-#if !__LINE__
-namespace DDWrapper {
-public 
-
-#endif
-
-enum JsonKeyValues : int
-{
-	JSON_DEBRIS_NUMBER = 0,
-    JSON_ISS_ONE,
-	JSON_TOTAL_KEY_COUNT
-};
-
-#if !__LINE__
-}
-#endif
